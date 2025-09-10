4b959a92519a08f95f914c6a5601abf002307390
Vizo2104 <tranxuanvuong21092004@gmail.com>
2025-09-10 23:26:48 +0200

    Update dic.py

--- dic.py
+++ dic.py
@@ -2,3 +2,5 @@ my_dict = { 'a': 1, 'b': 2, 'c': 3}  # 1 je hodnota pro klíč  'a'
 print(my_dict['a'])
 my_dict['d'] = '4' #přídání nového klíče 'd ' s hodnotou 4
 print(my_dict)
+my_dict['e']= '5'
+print(my_dict)
