d8519225f9b0b0eb7b25a7aacfdc0d4e59f35c90
Vizo2104 <tranxuanvuong21092004@gmail.com>
2025-09-10 23:45:16 +0200

    Delete dic.py

--- dic.py
+++
@@ -1,6 +0,0 @@
-my_dict = { 'a': 1, 'b': 2, 'c': 3}  # 1 je hodnota pro klíč  'a'
-print(my_dict['a'])
-my_dict['d'] = '4' #přídání nového klíče 'd ' s hodnotou 4
-print(my_dict)
-my_dict['e']= '5'
-print(my_dict)
